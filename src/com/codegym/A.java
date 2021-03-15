5d640f907d13fe43b0941af0c4c9e637217cc675
quan200699 <51079189+quan200699@users.noreply.github.com>
2021-03-15 15:59:45 +0700

    change in class A

---
+++ src/com/codegym/A.java
@@ -0,0 +1,10 @@
+package com.codegym;
+
+public class A {
+    static int x = 5;
+    int c = 3;
+
+    public static void main(String[] args) {
+        System.out.println(x);
+    }
+}
--- src/com/codegym/Main.java
+++ src/com/codegym/Main.java
@@ -3,6 +3,5 @@ package com.codegym;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
     }
 }
